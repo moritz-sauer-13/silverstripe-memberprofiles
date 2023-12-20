566e685ddf9719d60b43c3a4d3d7e68d3b1ef4b6
Moritz Sauer <43135946+moritz-sauer-13@users.noreply.github.com>
2023-12-20 12:50:28 +0100

    Update MemberProfilesAddSectionAction.php

--- src/Forms/MemberProfilesAddSectionAction.php
+++ src/Forms/MemberProfilesAddSectionAction.php
@@ -44,8 +44,8 @@ class MemberProfilesAddSectionAction extends GridFieldDetailForm implements Grid
             $links[Controller::join_links($base, $class)] = $title;
         }
 
-        Requirements::javascript('symbiote/silverstripe-memberprofiles: client/javascript/MemberProfilesAddSection.js');
-        Requirements::css('symbiote/silverstripe-memberprofiles: client/css/MemberProfilesAddSection.css');
+        Requirements::javascript('moritz-sauer-13/silverstripe-memberprofiles: client/javascript/MemberProfilesAddSection.js');
+        Requirements::css('moritz-sauer-13/silverstripe-memberprofiles: client/css/MemberProfilesAddSection.css');
 
         $select = new DropdownField("{$grid->getName()}[SectionClass]", '', $links);
         $select->setEmptyString(_t('MemberProfiles.SECTIONTYPE', '(Section type)'));
